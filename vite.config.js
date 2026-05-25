9a75cc4f97fa342815f36d44dc9905a8b0583e49
Sand m <maesand@yandex.ru>
2026-05-25 23:54:09 +0700

    Fix

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  base: '/Indie-Wave-master/',
+  base: './',
   build: {
     rollupOptions: {
       input: {
